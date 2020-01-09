77abda750f9a7f51605af1d47e81db92526cb2aa
NehemiahG7 <nehemiahgene@gmail.com>
2020-01-09 16:02:46 -0600

    Refactored file management and user input
    Main now creates a inventory csv if one does not already exist, and user input is seperated into catagories based on food group

--- main.go
+++ main.go
@@ -5,28 +5,50 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
-func main() {
+func getInv() {
 	scanner := bufio.NewScanner(os.Stdin)
 	inv, err := os.Create("inv.csv")
 	if err != nil {
-		log.Fatalf("failed creating file: %s", err)
+		log.Fatalf("File failed to create %s", err)
 	}
 
-	fmt.Println("Enter what you have in your pantry seperated by comas: ")
-	scanner.Scan()
-	input := scanner.Text()
-	fmt.Println(input)
-	arry := strings.Split(input, ",")
-
-	for i := 0; i < len(arry); i++ {
-		num, err := inv.WriteString(arry[i])
-		if err != nil {
-			log.Fatalf("failed creating file: %s", err)
+	var group string
+	var input string
+	for i := 0; i < 5; i++ {
+		switch i {
+		case 0:
+			group = "meats"
+		case 1:
+			group = "fruits"
+		case 2:
+			group = "vegetables"
+		case 3:
+			group = "grains"
+		case 4:
+			group = "dairy"
 		}
-		fmt.Println(num)
+		fmt.Printf("Enter any %s that you have. Seperate each by only a comma: ", group)
+		scanner.Scan()
+		input += group + "," + scanner.Text() + "\n"
+	}
+	fmt.Println(input)
+	num, err := inv.WriteString(input)
+	if err != nil {
+		fmt.Println(err, num)
+	}
+	inv.Close()
+}
+
+func main() {
+	//scanner := bufio.NewScanner(os.Stdin)
+	inv, err := os.Open("inv.csv")
+
+	if err != nil {
+		//Initialize .csv if inv has not been taken before
+		fmt.Println("It looks like you haven't used kitchen manager before. Lets take your inv:")
+		getInv()
 	}
 	inv.Close()
 
